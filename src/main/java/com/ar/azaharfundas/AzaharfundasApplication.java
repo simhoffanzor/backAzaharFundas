cc9809af9d9e389e42c74ad5d0621d225fae4910
simhoffanzor <97742785+simhoffanzor@users.noreply.github.com>
2022-06-11 23:54:05 -0300

    CORS Firebase

--- src/main/java/com/ar/azaharfundas/AzaharfundasApplication.java
+++ src/main/java/com/ar/azaharfundas/AzaharfundasApplication.java
@@ -18,7 +18,7 @@ public class AzaharfundasApplication {
             return new WebMvcConfigurer(){
                 @Override
                 public void addCorsMappings(CorsRegistry registry){
-                    registry.addMapping("/**").allowedOrigins("http://localhost:4200/").allowedHeaders("*").allowedMethods("*");
+                    registry.addMapping("/**").allowedOrigins("https://azahar-fundas.firebaseapp.com").allowedHeaders("*").allowedMethods("*");
                     
                 }
             };
